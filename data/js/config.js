3e674a7f0a0dc0a4e2948ec870b0e3c2bfbf80d9
Zaky97 <zaky7645@gmail.com>
2023-10-25 00:23:51 +0700

    new file:   README.md 	new file:   chat/css/style.css 	new file:   chat/index.html 	new file:   chat/js/script.js 	new file:   dashboard/css/style.css 	new file:   dashboard/css/wide.css 	new file:   dashboard/index.html 	new file:   dashboard/js/script.js 	new file:   data/Fonts/SegoeUI/Segoe_UI.ttf 	new file:   data/Fonts/SegoeUI/Segoe_UI_Bold.ttf 	new file:   data/Fonts/SegoeUI/Segoe_UI_Bold_Italic.ttf 	new file:   data/Fonts/SegoeUI/Segoe_UI_Italic.ttf 	new file:   data/css/default.css 	new file:   data/css/styles.css 	new file:   data/css/styles.css.map 	new file:   data/css/styles.scss 	new file:   data/helper/audio/audio.css 	new file:   data/helper/audio/audio.js 	new file:   data/helper/file/file.js 	new file:   data/helper/popup/popup.css 	new file:   data/helper/popup/popup.js 	new file:   data/img/devanka_bot.jpg 	new file:   data/img/error_image.jpg 	new file:   data/img/firebase_banner.png 	new file:   data/img/font_awesome_banner.png 	new file:   data/img/group.jpg 	new file:   data/img/kirimin_chat.png 	new file:   data/img/kirimin_chat_lighter.png 	new file:   data/img/kirimin_chat_no_fill.png 	new file:   data/img/kirimin_logo.png 	new file:   data/img/kontrolin_banner.jpg 	new file:   data/img/notipin_banner.jpg 	new file:   data/img/profile.jpg 	new file:   data/img/wave.svg 	new file:   data/js/authState.js 	new file:   data/js/config.js 	new file:   data/js/language.json 	new file:   data/js/userState.js 	new file:   favicon.ico 	new file:   group/create/css/style.css 	new file:   group/create/index.html 	new file:   group/create/js/script.js 	new file:   group/css/style.css 	new file:   group/index.html 	new file:   group/js/script.js 	new file:   group/setting/index.html 	new file:   group/setting/js/script.js 	new file:   index.html 	new file:   login/css/style.css 	new file:   login/email-verification.html 	new file:   login/index.html 	new file:   login/js/email-verification.js 	new file:   login/js/script.js 	new file:   post/create/css/style.css 	new file:   post/create/index.html 	new file:   post/create/js/script.js 	new file:   post/index.html 	new file:   user/css/style.css 	new file:   user/index.html 	new file:   user/js/script.js 	new file:   user/setting/css/style.css 	new file:   user/setting/css/wide.css 	new file:   user/setting/index.html 	new file:   user/setting/js/script.js

---
+++ data/js/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsqpCNfqBMvULww4NMiHkhsZtnxkd4UT4",
+    authDomain: "zcompany-47d1a.firebaseapp.com",
+    databaseURL: "https://zcompany-47d1a-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "zcompany-47d1a",
+    storageBucket: "zcompany-47d1a.appspot.com",
+    messagingSenderId: "111216650120",
+    appId: "1:111216650120:web:2098dd6e6ecf0eb3d4fc3d",
+    measurementId: "G-VHD0YVH5P2"
+};
