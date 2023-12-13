97082b79252b92f434ce66af29d646f2c539b2f2
Muhammad Zaky <131351380+Zaky97@users.noreply.github.com>
2023-12-13 15:35:26 +0700

    UPDATE

--- data/js/config.js
+++ data/js/config.js
@@ -1,10 +1,25 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAsqpCNfqBMvULww4NMiHkhsZtnxkd4UT4",
-    authDomain: "zcompany-47d1a.firebaseapp.com",
-    databaseURL: "https://zcompany-47d1a-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "zcompany-47d1a",
-    storageBucket: "zcompany-47d1a.appspot.com",
-    messagingSenderId: "111216650120",
-    appId: "1:111216650120:web:2098dd6e6ecf0eb3d4fc3d",
-    measurementId: "G-VHD0YVH5P2"
+    apiKey: "AIzaSyAn-pknrkw-aZJLSUtBo1toJ-nOsVSjYXw",
+    authDomain: "chat-923ba.firebaseapp.com",
+    databaseURL: "https://chat-923ba-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "chat-923ba",
+    storageBucket: "chat-923ba.appspot.com",
+    messagingSenderId: "359173041598",
+    appId: "1:359173041598:web:d28815db7431f81c8f9def",
+    measurementId: "G-L3EXLGFHXH"
 };
+
+// apiKey: "AIzaSyAn-pknrkw-aZJLSUtBo1toJ-nOsVSjYXw",
+
+// authDomain: "chat-923ba.firebaseapp.com",
+
+// projectId: "chat-923ba",
+
+// storageBucket: "chat-923ba.appspot.com",
+
+// messagingSenderId: "359173041598",
+
+// appId: "1:359173041598:web:d28815db7431f81c8f9def",
+
+// measurementId: "G-L3EXLGFHXH"
+ 
